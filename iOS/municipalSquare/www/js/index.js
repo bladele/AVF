a188781e0ee93e528755a718e65ac0b5d4056866
Bodunrin Ladele <bladele@Bodunrins-MacBook-Pro.local>
2013-06-19 18:46:30 -0400

    Instagram iOS API online.

--- iOS/municipalSquare/www/js/index.js
+++ iOS/municipalSquare/www/js/index.js
@@ -1,15 +1,48 @@
+
+
+function onload(){
+	document.addEventListener("deviceready", onDeviceReady, false);
+	
+}
+
+function onDeviceReady(){
+	
+}
+
+
+
+//Instagram function
+$("#instaBtn").click(function(){
+	var tagName = $("#instgrmQry").val();
+	var url = "https://api.instagram.com/v1/tags/" + tagName + "/media/recent?callback=?&amp;client_id=7b67e5d2940a4d3f982644f6778dd421";
+	
+	$.getJSON(url, pics);
+		
+});
+
+var pics = function(info){
+	
+	console.log(info);
+	
+	$.each(info.data, function(index, photo){
+		var pic = "<li><img src=' " + photo.images.low_resolution.url + " ' alt=' " + photo.user.id + " ' /></li>";
+		
+		$("#instgrmImgs").append(pic);
+	});
+	
+};
+
+
+
 //json-p
-
-$(function(){
-
-	var pop = "popular"; 
-	var url = "https://api.instagram.com/v1/media/" + pop + "/popular?callback=?&amp;client_id=14d49ad9fc264835b0c6f0722bdaf220";
+/*$('#media').on('pageinit', function(){
+	
+	var tagName = "kittens"; 
+	var url = "https://api.instagram.com/v1/tags/" + tagName + "/media/recent?callback=?&amp;client_id=7b67e5d2940a4d3f982644f6778dd421";
 	
 	$.getJSON(url, screenOutput);
 	
-});	
-	
-var screenOutput = function(info){
+	var screenOutput = function(info){
 	
 	alert("screenOutput");
 	console.log(info);
@@ -21,10 +54,22 @@ var screenOutput = function(info){
 		var pic = "<li><img src='" + photo.images.standard_resoltion.url + "' alt='" + photo.user.id
 		 + "' /><h4" + photo.user.full_name + ", <em>(" + photo.user.username +")</em></h4></li>";
 		 	$("#inst-feed").append(pic);
+		 	
+		 	$("#inst-feed").append(pic);
 	
 	});
 
 };
+	
+});
+
+
+*/
+
+
+
+
+
 
 
 	
