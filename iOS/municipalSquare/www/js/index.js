84ef3d5eac6a5daae88ce87f80d8bce55f46b909
Bodunrin Ladele <bladele@Bodunrins-MacBook-Pro.local>
2013-06-20 22:00:29 -0400

    3 native features and 1 API online. Just started having issues with the Android simulator, and I can't find what the problem is.

--- iOS/municipalSquare/www/js/index.js
+++ iOS/municipalSquare/www/js/index.js
@@ -1,74 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-function onload(){
-	document.addEventListener("deviceready", onDeviceReady, false);
-	
-}
-
-function onDeviceReady(){
-	console.log("All systems are go!");
-	
-}
-
-
-
-//Instagram Search
-$("#instaBtn").click(function(){
-	$("#instgrmImgs").empty();//Remove all current images
-	var tagName = $("#instgrmQry").val();
-	var url = "https://api.instagram.com/v1/tags/" + tagName + "/media/recent?callback=?&amp;client_id=7b67e5d2940a4d3f982644f6778dd421";
-	
-	$.getJSON(url, pics);
-		
-});
-
-var pics = function(info){
-	
-	console.log(info);
-	
-	$.each(info.data, function(index, photo){
-		var pic = "<li><img src=' " + photo.images.low_resolution.url + " ' alt=' " + photo.user.id + " ' /></li>";
-		
-		$("#instgrmImgs").append(pic);
-	});
-	
+        console.log('Received Event: ' + id);
+    }
 };
 
 
 
-//Geolocation
-$(document).on("pageinit", function(){
-	
-	$("#townMap").on("pageshow", function(){
-		navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
-	});
-	
-});
 
 
-function onGeoSuccess(position) {
-			lat = position.coords.latitude;
-			lon = position.coords.longitude;
-			var currentposition = new google.maps.LatLng(lat,lon);
-			
-			var mapoptions = {
-				zoom: 12,
-				center: currentposition,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
-			
-			var map = new google.maps.Map(document.getElementById("map"), mapoptions);
-			
 
-			var marker = new google.maps.Marker({
-					position: currentposition,
-					map: map
-			});
-		}
 
-function onGeoError(error){
-	if(error == 1){
-		alert("Please turn your GPS on.");
-	}
-}
+
+
+
 
